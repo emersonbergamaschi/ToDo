56a53b3b7d9f56c01b3afea58f61348005890d6b
Emerson Bergamaschi <emerson.bergamaschi@gmail.com>
2020-04-19 17:57:19 -0300

    Projeto To Do

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('helloworld/', views.helloword),
     path('', views.tasklist, name='task-list'),
     path('task/<int:id>', views.taskview, name='task-view'),
+    path('newtask/', views.newtask, name='new-task'),
+    path('edit/<int:id>', views.edittask, name='edit-task'),
+    path('delete/<int:id>', views.deletetask, name='delete-task'),
     path('yourname/<str:name>', views.yourname, name='your-name')
 ]
